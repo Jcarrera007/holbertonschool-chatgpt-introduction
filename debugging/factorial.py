cf90460108d488976c2122c9c4a9a2a34287bcef
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-05 13:00:01 -0400

    update 1

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,8 +5,21 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n -= 1  # Correction: decrement n to avoid infinite loop
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <integer>")
+        sys.exit(1)
+
+    try:
+        num = int(sys.argv[1])
+        if num < 0:
+            raise ValueError("Negative numbers don't have factorials.")
+    except ValueError as ve:
+        print(f"Error: {ve}")
+        sys.exit(1)
+
+    f = factorial(num)
+    print(f)
